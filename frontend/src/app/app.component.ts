a21d9657511143434ca368ec33bab593bfde96c6
Marshal-Emanuel <marshcodes@gmail.com>
2024-07-09 18:41:52 +0300

    landing page check

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/src/app/components/landing-page/landing-page.component.ts
+++ frontend/src/app/components/landing-page/landing-page.component.ts
@@ -1,9 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
+interface Review {
+  reviewerName: string;
+  reviewerImg: string;
+  reviewText: string;
+  starRating: number;
+}
+
+interface EventOrganizer {
+  name: string;
+  profileImg: string;
+}
+
+interface UpcomingEvent {
+  eventName: string;
+  eventImg: string;
+  description: string;
+  location: string;
+  date: string;
+  attendees: string;
+  phoneNumber: string;
+}
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
